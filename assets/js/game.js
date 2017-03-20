17417bff05333009b4630978a30726ba5d4a852a
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-03-19 20:02:44 -0700

    chat functionality with enter / leave status messages and working firebase presence, new players can replace recently left players

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,10 +14,13 @@ var playersRef = firebase.database().ref("players");
 var p1Ref = firebase.database().ref("players/1");
 var p2Ref = firebase.database().ref("players/2");
 var chat = firebase.database().ref("chat");
+var connectedRef = firebase.database().ref(".info/connected");
+var connectionsRef = firebase.database().ref("connections");
 
 //Local vars
 var playerName;
 var playerNum;
+var playerKey; // unique to session client
 var turn = 1;
 var timeDelay = 4000;
 
@@ -33,10 +36,39 @@ var displayChoices = function(pNum) {
 	}
 }
 
+var displayGameMessage = function(type) {
+	if(playerNum === 1) {
+		if(type === "yourTurn") {
+			$("#game-message").text("It's Your Turn!");
+			$("#game-message").show();
+		} else if(type === "waitingFor") {
+			p2Ref.once("value", function(snap) {
+				if(snap.exists() === true) {
+					$("#game-message").text("Waiting for " + snap.val().name + " to choose...");
+				}
+			});
+			$("#game-message").show();
+		}
+	} else if(playerNum === 2) {
+		if(type === "yourTurn") {
+			$("#game-message").text("It's Your Turn!");
+			$("#game-message").show();
+		} else if(type === "waitingFor") {
+			p1Ref.once("value", function(snap) {
+				if(snap.exists() === true) {
+					$("#game-message").text("Waiting for " + snap.val().name + " to choose...");
+				}
+			});
+			$("#game-message").show();
+		}
+	}
+}
+
 //Player enter event listener
 $("#name-submit-button").click(function(e) {
 	e.preventDefault();
 	playerName = $("#player-name").val().trim();
+	$("#player-name").val("");
 
 	playersRef.once("value", function(snap) {
 		if(snap.exists() === false) {
@@ -44,24 +76,48 @@ $("#name-submit-button").click(function(e) {
 			p1Ref.update({
 				name: playerName,
 				wins: 0,
-				losses: 0
+				losses: 0,
+				key: userKey
 			});
+			connectionsRef.child(userKey).set(playerName);
+		} else if(snap.child(2).exists() === true && snap.child(1).exists() === false) {
+			playerNum = 1;
+			p1Ref.update({
+				name: playerName,
+				wins: 0,
+				losses: 0,
+				key: userKey
+			});
+
+			displayChoices(1);
+			connectionsRef.child(userKey).set(playerName);
 		} else {
 			playerNum = 2;
 			p2Ref.update({
 				name: playerName,
 				wins: 0,
-				losses: 0
+				losses: 0,
+				key: userKey
 			});
 			ref.update({
 				turn: 1
 			});
+			connectionsRef.child(userKey).set(playerName);
 		}
 	}).then(function() {
 		$("#p-name").text(playerName);
 		$("#p-num").text(playerNum);
 		$("#you-are-message").show();
-		$("#player-form").hide();
+		$("#enter-game-panel").hide();
+
+		var message = " [ HAS ENTERED THE GAME! ]";
+		var time = new Date().toLocaleString("en-US", {hour: "numeric", minute: "numeric", second: "numeric"});
+
+		chat.push({
+			name: playerName,
+			message: message,
+			time: time
+		});
 	});
 });
 
@@ -69,19 +125,48 @@ $("#name-submit-button").click(function(e) {
 p1Ref.child("name").on("value", function(snap) {
 	if(snap.exists() === true) {
 		$("#p1-name").text(snap.val());
+		$("#p1-name").addClass("p1-name-entered");
+		$("#p1-name").removeClass("p-not-entered");
 	}
 });
 
 p2Ref.child("name").on("value", function(snap) {
 	if(snap.exists() === true) {
 		$("#p2-name").text(snap.val());
+		$("#p2-name").addClass("p2-name-entered");
+		$("#p2-name").removeClass("p-not-entered");
+	}
+});
+
+// Firebase listener for win/loss values
+p1Ref.child("wins").on("value", function(snap) {
+	if(snap.exists() === true) {
+		$("#p1-wins").text(snap.val());
+	}
+});
+
+p1Ref.child("losses").on("value", function(snap) {
+	if(snap.exists() === true) {
+		$("#p1-losses").text(snap.val());
+	}
+});
+
+p2Ref.child("wins").on("value", function(snap) {
+	if(snap.exists() === true) {
+		$("#p2-wins").text(snap.val());
+	}
+});
+
+p2Ref.child("losses").on("value", function(snap) {
+	if(snap.exists() === true) {
+		$("#p2-losses").text(snap.val());
 	}
 });
 
 // hide new player input for 3rd parties
 playersRef.on("value", function(snap) {
 	if(snap.child(1).exists() === true && snap.child(2).exists() === true) {
-		$("#player-form").hide();
+		$("#enter-game-panel").hide();
 	}
 });
 
@@ -107,28 +192,16 @@ playersRef.on("value", function(snap) {
 playersRef.on("value", function(snap) {
 	//display game message when both players exist and choices alternate
 	if(playerNum === 1) {
-		if(snap.child(1).child("choice").exists() === false) {
-			$("#game-message").text("It's Your Turn!");
-			$("#game-message").show();
+		if(snap.child(2).exists() === true && snap.child(1).child("choice").exists() === false) {
+			displayGameMessage("yourTurn");
 		} else {
-			p2Ref.once("value", function(snap) {
-				if(snap.exists() === true) {
-					$("#game-message").text("Waiting for " + snap.val().name + " to choose...");
-				}
-			});
-			$("#game-message").show();
+			displayGameMessage("waitingFor");
 		}
 	} else if(playerNum === 2) {
 		if(snap.child(2).child("choice").exists() === false && snap.child(1).child("choice").exists() === false) {
-			p1Ref.once("value", function(snap) {
-				if(snap.exists() === true) {
-					$("#game-message").text("Waiting for " + snap.val().name + " to choose...");
-				}
-			});
-			$("#game-message").show();
+			displayGameMessage("waitingFor");
 		} else {
-			$("#game-message").text("It's Your Turn!");
-			$("#game-message").show();
+			displayGameMessage("yourTurn");
 		}
 	}
 });
@@ -310,6 +383,7 @@ chat.on("child_added", function(snap) {
 	var name = snap.val().name;
 	var time = snap.val().time;
 	var message = snap.val().message;
+
 	var li = $("<li class='message'>").text(": " + message + " ");
 	li.prepend($("<span class='name'>").text(name));
 	li.append($("<span class='timestamp'>").text(time));
@@ -333,7 +407,83 @@ $("#send-button").on("click", function(e) {
 		});
 
 		$("#message-view").animate({ scrollTop: $("#message-view")[0].scrollHeight}, 500);
+	}
 
-		$("#chat-message").val("");
+	$("#chat-message").val("");
+});
+
+// Firebase connection listener
+connectedRef.on("value", function(snap) {
+	if(snap.val()) {
+		var user = connectionsRef.push(true);
+		userKey = user.getKey();
+		user.onDisconnect().remove();
+	}
+});
+
+connectionsRef.on("child_removed", function(snap) {
+	var leftKey = snap.getKey();
+	var p1Key;
+	p1Ref.once("value", function(snap) {
+		if(snap.exists() === true) {
+			p1Key = snap.val().key;
+		}
+	});
+
+	if(leftKey === p1Key) {
+		p1Ref.remove();
+		p2Ref.child("choice").remove();
+		ref.child("turn").remove();
+		$("#p1-name").empty();
+		$("#p1-name").text("Waiting for Player 1");
+		$("#p1-name").removeClass("p1-name-entered");
+		$("#p1-name").addClass("p-not-entered");
+		$("#p1-choices").empty();
+		$("#p2-choices").empty();
+		$("#game-message").empty();
+
+		var message = " [ HAS LEFT THE GAME! ]";
+		console.log("p1 has left");
+		var time = new Date().toLocaleString("en-US", {hour: "numeric", minute: "numeric", second: "numeric"});
+
+		chat.push({
+			name: snap.val(),
+			message: message,
+			time: time
+		});
+	}
+});
+
+connectionsRef.on("child_removed", function(snap) {
+	var leftKey = snap.getKey();
+	var p2Key;
+
+	p2Ref.once("value", function(snap) {
+		if(snap.exists() === true) {
+			p2Key = snap.val().key;
+		}
+	});
+
+	if(leftKey === p2Key) {
+		p2Ref.remove();
+		p1Ref.child("choice").remove();
+		ref.child("turn").remove();
+		$("#p2-name").empty();
+		$("#p2-name").text("Waiting for Player 2");
+		$("#p2-name").removeClass("p2-name-entered");
+		$("#p2-name").addClass("p-not-entered");
+		$("#p1-choices").empty();
+		$("#p2-choices").empty();
+		$("#game-message").empty();
+
+		var message = " [ HAS LEFT THE GAME! ]";
+		console.log("p2 has left");
+		var time = new Date().toLocaleString("en-US", {hour: "numeric", minute: "numeric", second: "numeric"});
+
+		chat.push({
+			name: snap.val(),
+			message: message,
+			time: time
+		});
 	}
 });
